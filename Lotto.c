62f53977c0a9d13c6bcae7c7cba54610ea66ba82
GuyWasin <bu.wasin_st@tni.ac.th>
2016-12-16 04:04:48 +0000

    3 digit reward commit

--- Lotto.c
+++ Lotto.c
@@ -5,21 +5,36 @@
 
 struct lotto{
     int first_reward;
+    int _3digitReward[4];
 };
 
-
-   
 void printLotto(int reward){
     //return random number 6 digits
-    int num6 = reward+1;
+    int num6;
+    if(reward%100000%10000%10000%1000%100%10!=9)
+        num6 = reward+1;
+    else num6 = reward-1;
     printf("Your lotto's number is %d\n",num6);
+}
+
+void init(struct lotto *p){
+    srandom(time(NULL));    
+    p->first_reward=rand()%1000000;
+    int i;
+    for(i=0;i<4;i++){
+        p->_3digitReward[i]=rand()%1000;
+    }
 }
 
 int main() {
    struct lotto Lotto;
-srandom(time(NULL));
-Lotto.first_reward=rand()%1000000;
+   init(&Lotto);
    printf("Lotto\nFirst reward is %d\n",Lotto.first_reward);
+   int i;
+   printf("3 digits reward is ");
+   for(i=0;i<4;i++){
+       printf(" %d",Lotto._3digitReward[i]);
+   }printf("\n");
    //call function printLotto
    printLotto(Lotto.first_reward);
    return 0;
