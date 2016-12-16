383efa3e5e5bcbcdb212b93c1c0991d70168affc
GuyWasin <bu.wasin_st@tni.ac.th>
2016-12-16 03:24:29 +0000

    secound commit

--- Lotto.c
+++ Lotto.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+struct lotto{
+    int first_reward;
+};
 
 void printLotto(){
     //return random number 5 digits
 }
 
-main() {
-    printf("Lotto\n");
-    //call function printLotto
+int main() {
+   struct lotto Lotto;
+   srandom(time(NULL));
+   Lotto.first_reward=rand()%1000000;
+   printf("Lotto\nFirst reward is %d",Lotto.first_reward);
+   //call function printLotto
+   return 0;
 }
