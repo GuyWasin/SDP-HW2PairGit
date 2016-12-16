7b3b1618dbcaf68b11b112b7908c2034ed1aa894
thanaphoom <thanaphoom.tni.ce05@gmail.com>
2016-12-16 03:42:12 +0000

    first fix commit

--- Lotto.c
+++ Lotto.c
@@ -7,15 +7,20 @@ struct lotto{
     int first_reward;
 };
 
-void printLotto(){
-    //return random number 5 digits
+
+   
+void printLotto(int reward){
+    //return random number 6 digits
+    int num6 = reward+1;
+    printf("Your lotto's number is %d\n",num6);
 }
 
 int main() {
    struct lotto Lotto;
-   srandom(time(NULL));
-   Lotto.first_reward=rand()%1000000;
-   printf("Lotto\nFirst reward is %d",Lotto.first_reward);
+srandom(time(NULL));
+Lotto.first_reward=rand()%1000000;
+   printf("Lotto\nFirst reward is %d\n",Lotto.first_reward);
    //call function printLotto
+   printLotto(Lotto.first_reward);
    return 0;
 }
